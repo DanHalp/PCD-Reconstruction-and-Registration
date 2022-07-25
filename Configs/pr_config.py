6f12169b1a899db5c36c6020822f5539dabd68f6
Dan Halperin <dan_halperin2_gmail_com@deeplearning-1-vm.us-west1-b.c.triple-bonito-356109.internal>
2022-07-25 13:24:53 +0000

    Latests updates

--- Configs/pr_config.py
+++ Configs/pr_config.py
@@ -1,10 +1,18 @@
 from easydict import EasyDict as edict
+from enum import Enum
+import numpy as np
+
 
 CFG = edict()
 
+###################### MagicNumbers ######################
+class DATASETS(Enum):
+    SHAPENET = 0
+    MODELNET = 1
+
 ###################### SpareNet ######################
 CFG.RECONSTRUCTION = edict()
-CFG.RECONSTRUCTION.ckpt = "/home/danha/PCD-Reconstruction-and-Registration/data/Checkpoints/SpareNet/SpareNet.pth"
+CFG.RECONSTRUCTION.model_path = "/home/danha/PCD-Reconstruction-and-Registration/data/Checkpoints/SpareNet/SpareNet.pth"
 CFG.RECONSTRUCTION.model = ["sparenet"][0]
 CFG.RECONSTRUCTION.test_mode = ["ML3D", "default"][0]
 CFG.RECONSTRUCTION.gpu = ["0"][0]
@@ -12,37 +20,44 @@ CFG.RECONSTRUCTION.test_mode = ["ML3D", "default"][0]
 CFG.RECONSTRUCTION.sample_limit = [1][0]
 CFG.RECONSTRUCTION.random_samples = [False, True][0]
 CFG.RECONSTRUCTION.save = [False, True][0]
-CFG.RECONSTRUCTION.output = "/home/danha/PCD-Reconstruction-and-Registration/Outputs/Completion"
+CFG.RECONSTRUCTION.output = "/home/danha/PCD-Reconstruction-and-Registration/Outputs/"
 CFG.RECONSTRUCTION.local_dir = "/home/danha/PCD-Reconstruction-and-Registration/SpareNet"
+CFG.RECONSTRUCTION.active = [True, False][0]
+
 
 
 ######################### DCP #########################
-CFG.TRANSORM = edict()
-CFG.TRANSORM.output = CFG.RECONSTRUCTION.output
-CFG.TRANSORM.ckpt = "/home/danha/PCD-Reconstruction-and-Registration/DCP/pretrained/dcp_v1.t7"
-CFG.TRANSORM.exp_name = ["exp"][0]
-CFG.TRANSORM.model = ['dcp'][0]
-CFG.TRANSORM.emb_nn = ['pointnet', 'dgcnn'][0]
-CFG.TRANSORM.pointer = ['identity', 'transformer'][1]
-CFG.TRANSORM.head = ['mlp', 'svd'][1]
-CFG.TRANSORM.emb_dims = 512
-CFG.TRANSORM.n_blocks = 1
-CFG.TRANSORM.n_heads = 4
-CFG.TRANSORM.ff_dims = 1024
-CFG.TRANSORM.dropout = 0.0
-CFG.TRANSORM.batch_size = 32
-CFG.TRANSORM.test_batch_size = 10
-CFG.TRANSORM.epochs = 250
-CFG.TRANSORM.use_sgd = [True, False][1]
-CFG.TRANSORM.lr = 1e-1
-CFG.TRANSORM.momentum = 0.9
-CFG.TRANSORM.no_cuda = [True, False][1]
-CFG.TRANSORM.seed = 1234
-CFG.TRANSORM.eval = [True, False][1]
-CFG.TRANSORM.cycle = [True, False][1]
-CFG.TRANSORM.gaussian_noise = [True, False][1]
-CFG.TRANSORM.unseen = [True, False][1]
-CFG.TRANSORM.num_points = 1024
-CFG.TRANSORM.dataset = ['modelnet40', "ShapeNet"]
-CFG.TRANSORM.factor = 4
-CFG.TRANSORM.model_path = CFG.TRANSORM.ckpt
+CFG.TRANSFORM = edict()
+CFG.TRANSFORM.output = CFG.RECONSTRUCTION.output
+CFG.TRANSFORM.test_folder_name = ["Comparison"][0]
+CFG.TRANSFORM.model_path = "/home/danha/PCD-Reconstruction-and-Registration/DCP/pretrained/dcp_v2.t7"
+CFG.TRANSFORM.exp_name = ["dcp_v1", "dcp_v2"][1]
+CFG.TRANSFORM.model = ['dcp'][0]
+CFG.TRANSFORM.emb_nn = ['pointnet', 'dgcnn'][1]
+CFG.TRANSFORM.pointer = ['identity', 'transformer'][1]
+CFG.TRANSFORM.head = ['mlp', 'svd'][1]
+CFG.TRANSFORM.emb_dims = 512
+CFG.TRANSFORM.n_blocks = 1
+CFG.TRANSFORM.n_heads = 4
+CFG.TRANSFORM.ff_dims = 1024
+CFG.TRANSFORM.dropout = 0.0
+CFG.TRANSFORM.batch_size = 16
+CFG.TRANSFORM.test_batch_size = 20
+CFG.TRANSFORM.iterations = 5
+CFG.TRANSFORM.epochs = 1
+CFG.TRANSFORM.val_every = 5
+CFG.TRANSFORM.use_sgd = [True, False][1]
+CFG.TRANSFORM.lr = 1e-1
+CFG.TRANSFORM.momentum = 0.9
+CFG.TRANSFORM.no_cuda = [True, False][1]
+CFG.TRANSFORM.seed = 1234
+CFG.TRANSFORM.eval = [True, False][0]
+CFG.TRANSFORM.cycle = [True, False][1]
+CFG.TRANSFORM.gaussian_noise = [True, False][1]
+CFG.TRANSFORM.unseen = [True, False][1]
+CFG.TRANSFORM.num_points = [512, 1024, 2048, 3072, 4096, 16384][1]
+CFG.TRANSFORM.factor = 4
+CFG.TRANSFORM.pc_type = ["gtcloud", "partial_cloud"][1]   # Relevant only to ShapeNet
+CFG.TRANSFORM.dataset = [DATASETS.SHAPENET, DATASETS.MODELNET][0]
+CFG.TRANSFORM.dataset_name = "ShapeNet" if  CFG.TRANSFORM.dataset == DATASETS.SHAPENET else "ModelNet"
+CFG.TRANSFORM.fixed_dataset_indices = [np.array([295, 944,  869,  465,  997,  906,  735, 1068,  725, 1138,  573,  374,  482,  912, 737, 1175,  210,  351,  705, 11]), None][0]
--- main.py
+++ main.py
@@ -4,30 +4,86 @@ import os
 import shutil
 import open3d
 import numpy as np
+from datetime import datetime
 from scipy.spatial.transform import Rotation as Rot
 from tqdm import tqdm
 import time
 from Configs.pr_config import CFG
-
+import torch
 from SpareNet.Inference_model import SpareNet
+from DCP.inference_model import DCP_MODEL
+from easydict import EasyDict as edict
+from SpareNet.configs.base_config import cfg as sparenet_cfg
+from DCP.main import main_func as dcp_main
 
 
+class IOStream:
+    def __init__(self, path):
+        self.f = open(path, 'a')
+
+    def cprint(self, text):
+        print(text)
+        self.f.write(text + '\n')
+        self.f.flush()
+        
+    def write_file(self, text):
+        self.f.write(text + '\n')
+        self.f.flush()
+
+    def close(self):
+        self.f.close()
+
+
+def make_dir(path):
+
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path, exist_ok=True)
 
 def create_model():
 
-
-    m = SpareNet(args=CFG)
-    m.test()
     
-    # Perfom DCP on predicted:
-    # path = Path(args.TRNSFORM.output) / args.model / "point_clouds"
-    # samples_list = os.listdir(path)
-    # bound = args.sample_limit if args.sample_limit != -1 else len(samples_list)
-    # for i in range(bound):
-    #     path = path / samples_list[i]
-    #     pcds = os.listdir(path)
-
+    if "PROJECT" not in CFG:
+        CFG.PROJECT = edict()
+    CFG.update(sparenet_cfg)
     
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    CFG.PROJECT.device = device
+    CFG.TRAIN.batch_size = CFG.TRANSFORM.batch_size
+    
+    ################################# Training #################################
+    # model = DCP_MODEL(args=CFG, partiton="train")
+
+    # model.train()
+    # dcp_main(CFG)
+    
+    ################################# Testing + Metrics saving #################################
+    
+    cloud_type = "Reconstruction" if CFG.RECONSTRUCTION.active else CFG.TRANSFORM.pc_type
+    pc_path = Path(CFG.TRANSFORM.output) / CFG.TRANSFORM.dataset_name / cloud_type
+    make_dir(pc_path)
+    metrics_path = Path(CFG.TRANSFORM.output) / "Metrics" / cloud_type
+    make_dir(metrics_path)
+    CFG.TRANSFORM.output = pc_path
+    CFG.TRANSFORM.metrics_path = metrics_path
+    
+    time = datetime.now().strftime('run_%H_%M_%d_%m_%Y')
+    metrics_file = IOStream(metrics_path / f"metrics_{CFG.TRANSFORM.dataset_name}_{cloud_type}_{time}.csv")
+    metrics_file.write_file("Iteration,ID,Loss,Cycle_Loss,MSE,RMSE,MAE,rot_MSE,rot_RMSE,rot_MAE,trans_MSE,trans_RMSE,trans_MAE")
+    xxx_file = IOStream(metrics_path / f"transformation_{CFG.TRANSFORM.dataset_name}_{cloud_type}_{time}.csv")
+    xxx_file.write_file("Sample_ID;type;rotation_ab;translation_ab;rotation_ba;translation_ba")
+
+
+    model = DCP_MODEL(args=CFG)
+    
+    torch.cuda.empty_cache()
+    with torch.no_grad():
+        model.test_print(metrics_file, xxx_file)
+
+    metrics_file.close()
+    xxx_file.close()
+   
+
     
     
     
